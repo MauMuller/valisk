8b51addabf41293c290f1b5147880d3fd7aa1fce
MauMuller <82516932+MauMuller@users.noreply.github.com>
2022-12-22 10:33:10 -0300

    Finalizado os testes para o hook de password utilizando o hook único

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { useMaskCPF } from "./hooks/useMaskCPF";
 import { useMaskCNPJ } from "./hooks/useMaskCNPJ";
 import { useMaskCEP } from "./hooks/useMaskCEP";
@@ -6,25 +8,20 @@ import { useMaskPhone } from "./hooks/useMaskPhone";
 import { useMaskPassword } from "./hooks/useMaskPassword";
 
 import { useMasks } from "./hooks/useMasks";
-import { useState } from "react";
 
 function App() {
-  const [hiddenValue, setHiddenValue] = useState(true);
+  const [hiddenValue, setHiddenValue] = useState(false);
 
-  const [valuesPassword, setPassword, isValidPassword] = useMaskPassword({
-    inicialValue: "10",
-    hideValue: hiddenValue,
-    passwordPontenciality: { numbers: [5], specialChars: [1, "#"] },
+  const { values, setValues, isValidValues, setKeys } = useMasks({
+    password: { hideValue: hiddenValue },
   });
 
-  // const [cnpjValue, cpfValue, phoneValue] = values;
-  // const [setCNPJ, setCPF, setPhone] = setValues;
-  // const [setKeysCNPJ, setKeysCPF, setKeysPhone] = setKeys;
+  const [valuesPassword] = values;
+  const [setPassword] = setValues;
 
   const [value, sourceValue] = valuesPassword;
 
   console.clear();
-  console.log(JSON.stringify(isValidPassword, null, "\t"));
   console.log(sourceValue);
 
   return (
@@ -34,28 +31,6 @@ function App() {
         value={value}
         onChange={(evt) => setPassword(evt.target.value)}
       />
-
-      {/* <input
-        type="text"
-        value={cpfValue}
-        onChange={(evt) => setCPF(evt.target.value)}
-        onKeyDown={(evt) => setKeysCPF(evt.key)}
-      />
-
-      <input
-        type="text"
-        value={cnpjValue}
-        onChange={(evt) => setCNPJ(evt.target.value)}
-        onKeyDown={(evt) => setKeysCNPJ(evt.key)}
-      />
-
-      <input
-        type="text"
-        value={phoneValue}
-        onChange={(evt) => setPhone(evt.target.value)}
-        onKeyDown={(evt) => setKeysPhone(evt.key)}
-      /> 
-    */}
 
       <button onClick={() => setHiddenValue(!hiddenValue)}>Mostrar</button>
     </div>
