47ff3f756444e872a3f3358c63ecc267a1910850
Mauricio Muller dos Santos <mauriciomuller2012@gmail.com>
2022-12-26 22:49:47 -0300

    COndicionais do script atualizadas, agoa podendo ser usadas com a versão 'v1.0.0'

--- scripts/updatePackage.sh
+++ scripts/updatePackage.sh
@@ -7,5 +7,6 @@ secondValue=${versionNumber:2:1};
 thirtyValue=${versionNumber:4:1};
 
 if [[ $firstValue =~ [0] ]]; then npm version major; fi;
-if [[ $thirtyValue =~ [0-8] ]] && [ $firstValue  !=  '0' ]; then npm version patch; else npm version minor; fi;
 if [[ $secondValue =~ [9] ]]; then npm version major; fi;
+if [[ $thirtyValue =~ [0-8] ]] && [ $firstValue != [0] ]; then npm version patch; fi;
+if [[ $thirtyValue =~ [9] ]]; then npm version minor; fi;
