944b4771e4058d14d3c6cda8236da816d7b0c710
MauMuller <82516932+MauMuller@users.noreply.github.com>
2022-12-27 15:32:48 -0300

    Ajuste de design para ilustração da biblioteca

---
+++ README.md
@@ -0,0 +1,46 @@
+<div  
+  style="
+    display:flex;
+    align-items:center;
+    justify-content:center;
+    gap:1rem;
+    padding-bottom: 0.5rem;
+    margin-bottom: 0.8rem;
+    border-bottom: 1px solid #696969
+">
+  <img 
+    src="https://ik.imagekit.io/e6khzhxvx/Valisk?ik-sdk-version=javascript-1.4.3&updatedAt=1672157447910" 
+    width="105"
+  />
+  <div style="width:25rem;">
+    <span style="
+      font-weight:bold; 
+      font-size:1.5rem;
+      color:white;"
+    >
+      React Valisk
+    </span>
+    <p style="font-size:0.8rem">
+      Trata-se de uma biblioteca com a finalidade de proporcionar mascaras e validações para os campos de texto, tudo por meio de HOOKS, visando as tipagens e variadas opções para personalização.
+    </p>
+  </div>
+</div>
+
+<div style="
+  padding-bottom: 0.8rem;
+  display: flex;
+  gap: 0.5rem;
+  justify-content: center;
+">
+  <img alt="GitHub package.json version" src="https://img.shields.io/github/package-json/v/maumuller/react-valisk?color=%23ff1f3d&label=npm&style=for-the-badge" width="90">
+
+  <img alt="GitHub Workflow Status" src="https://img.shields.io/github/actions/workflow/status/maumuller/react-valisk/publish-package-and-release.yml?color=black&style=for-the-badge" width="105">
+
+  <img alt="GitHub" src="https://img.shields.io/github/license/maumuller/react-valisk?color=%23ff1f3d&style=for-the-badge" width="90">
+
+  <img alt="GitHub code size in bytes" src="https://img.shields.io/github/languages/code-size/maumuller/react-valisk?color=black&style=for-the-badge" width="120">
+</div>
+
+<div align="center">
+  <img src="https://ik.imagekit.io/e6khzhxvx/valisk-preview.png?ik-sdk-version=javascript-1.4.3&updatedAt=1672165728023" width="500">
+</div>
--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,20 @@ import { useMasks } from "./hooks/useMasks";
 function App() {
   const id = useId();
   const [hiddenValue, setHiddenValue] = useState(true);
+  const corretValidationURL =
+    "https://cdn-icons-png.flaticon.com/512/4436/4436481.png";
+
+  const invalidValidationURL =
+    "https://cdn.pixabay.com/photo/2017/03/28/01/42/attention-2180765_1280.png";
 
   const namesForLabes = ["password", "cep", "cnpj", "cpf"];
 
   const inputsMasks = namesForLabes.reduce((prev, current) => {
     const inicialObject = { inicialValue: "" };
     const passwordObj = { ...inicialObject, hideValue: hiddenValue };
-    const data =
-      current === "password" ? passwordObj : { useExplictMask: true };
+    const othersObjects =
+      current === "cpf" ? { useExplictMask: false } : { useExplictMask: true };
+    const data = current === "password" ? passwordObj : othersObjects;
 
     return { ...prev, [current]: data };
   }, {});
@@ -22,13 +28,16 @@ function App() {
     const valueInput = typeof value === "string" ? value : value.at(0);
     const setValue = setValues[indValue];
     const setKey = setKeys.at(indValue);
+    const isValid = isValidValues[indValue];
 
     const nameLabel = namesForLabes.at(indValue);
     const idConect = `${id}-${nameLabel}`;
 
     const passwordVisbilityBtn =
       nameLabel === "password" ? (
-        <button onClick={() => setHiddenValue(!hiddenValue)}>Mostrar</button>
+        <button onClick={() => setHiddenValue(!hiddenValue)}>
+          {hiddenValue ? "Mostrar" : "Ocultar"}
+        </button>
       ) : null;
 
     return (
@@ -41,8 +50,12 @@ function App() {
           onChange={(evt) => setValue(evt.target.value)}
           onKeyDown={(evt) => setKey?.(evt.key)}
         />
-
         {passwordVisbilityBtn}
+        {isValid ? (
+          <img alt="correct" src={corretValidationURL} width="30" />
+        ) : (
+          <img alt="invalid" src={invalidValidationURL} width="30" />
+        )}
       </div>
     );
   });
