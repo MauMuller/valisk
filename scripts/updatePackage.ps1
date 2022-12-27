7dfb391d82c02e4f0e0253d2956fe12991c5fc57
Mauricio Muller dos Santos <mauriciomuller2012@gmail.com>
2022-12-26 23:04:13 -0300

    Incremento de condicional para publicação do pacote e ajuste no script de windows

--- scripts/updatePackage.ps1
+++ scripts/updatePackage.ps1
@@ -4,14 +4,14 @@ $firstValue=$versionNumber[0];
 $secondValue=$versionNumber[2];
 $thirtyValue=$versionNumber[4];
 
-if($firstValue -eq '0') { 
-  npm version major 
-}
-
 if($thirtyValue -match '[0-8]' -and $firstValue -ne '0') { 
   npm version patch;
 } else {
   npm version minor;
+}
+
+if($firstValue -eq '0') { 
+  npm version major 
 }
 
 if($secondValue -eq '9') { 
