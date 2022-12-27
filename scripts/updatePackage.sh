ec1f154ae7c78f13013ec15a2cb30f65c3eb7fcb
Mauricio Muller dos Santos <mauriciomuller2012@gmail.com>
2022-12-26 22:59:13 -0300

    alteração na ordem das condicionais

--- scripts/updatePackage.sh
+++ scripts/updatePackage.sh
@@ -6,7 +6,7 @@ firstValue=${versionNumber:0:1};
 secondValue=${versionNumber:2:1};
 thirtyValue=${versionNumber:4:1};
 
+if [[ $thirtyValue =~ [0-8] ]] && [ $firstValue -ge 1 ]; then npm version patch; fi;
 if [[ $firstValue =~ [0] ]]; then npm version major; fi;
 if [[ $secondValue =~ [9] ]]; then npm version major; fi;
-if [[ $thirtyValue =~ [0-8] ]] && [ $firstValue -ge '2' ]; then npm version patch; fi;
 if [[ $thirtyValue =~ [9] ]]; then npm version minor; fi;
