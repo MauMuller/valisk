8f26c4b26ddc48ddfb1f07c29261fc0c1c76759e
MauMuller <82516932+MauMuller@users.noreply.github.com>
2023-01-03 08:27:38 -0300

    🔨 (updatePackeage.json): Incremento de mensagem de erro e sucesso para script
    A ideia é mostrar qual foi a situação gerada pelo script, mostrando o que é preciso fazer

--- scripts/updatePackage.ts
+++ scripts/updatePackage.ts
@@ -5,11 +5,23 @@ const numbers = version.split(".");
 
 const [major, minor, patch] = numbers;
 
+const errorMessage = "Faça commit dos arquivos no modo staged.";
+const sucessMessage = "A versão do pacote foi atualizada.";
+
 if (patch.search(/[0-8]/g) != -1 && major.search(/[1-9]/g) != -1)
-  exec("npm version patch");
+  exec("npm version patch", (err) =>
+    console.log(err ? errorMessage : sucessMessage)
+  );
 
-if (patch === "9") exec("npm version minor");
-if (minor === "9") exec("npm version major");
-if (major === "0") exec("npm version major");
-
-console.log(major);
+if (patch === "9")
+  exec("npm version minor", (err) =>
+    console.log(err ? errorMessage : sucessMessage)
+  );
+if (minor === "9")
+  exec("npm version major", (err) =>
+    console.log(err ? errorMessage : sucessMessage)
+  );
+if (major === "0")
+  exec("npm version major", (err) =>
+    console.log(err ? errorMessage : sucessMessage)
+  );
