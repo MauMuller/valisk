fcb558da827957ee81c9f3411c9e9e79cf2809cd
MauMuller <82516932+MauMuller@users.noreply.github.com>
2023-01-03 08:22:00 -0300

    🔖 (updatePackage.ts & package.json): Tentativa de realizar um script em ts para upgrade de versão do package
    A ideia desse script é substituir a necessidade de possuir dois scripts, um para cada SO

--- scripts/updatePackage.ts
+++ scripts/updatePackage.ts
@@ -5,29 +5,11 @@ const numbers = version.split(".");
 
 const [major, minor, patch] = numbers;
 
-if (patch.search(/[0-8]/g) != -1 && major.search(/[1-9]/g))
-  exec("npm version patch", (err) =>
-    err ? "Erro ao atualizar o patch" : "Patch atualizado com sucesso!"
-  );
+if (patch.search(/[0-8]/g) != -1 && major.search(/[1-9]/g) != -1)
+  exec("npm version patch");
 
-if (patch === "9")
-  exec("npm version minor", (err) =>
-    console.log(
-      err ? "Erro ao atualizar o minor" : "Minor atualizado com sucesso!"
-    )
-  );
+if (patch === "9") exec("npm version minor");
+if (minor === "9") exec("npm version major");
+if (major === "0") exec("npm version major");
 
-if (minor === "9")
-  exec("npm version major", (err) =>
-    console.log(
-      err ? "Erro ao atualizar o major" : "Major atualizado com sucesso!"
-    )
-  );
-
-if (major === "0") {
-  exec("npm version major", (err, output) => {
-    if (err) throw new Error("Erro");
-
-    console.log(output);
-  });
-}
+console.log(major);
